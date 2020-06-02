ea78a47bea8f3ae83c473715c84e4f18c47cdabc
ilya-varchenya <ilya.varchenya@gmail.com>
2020-06-02 13:28:00 +0300

    transform to Integer

--- src/org/foo/Point.groovy
+++ src/org/foo/Point.groovy
@@ -2,9 +2,11 @@ package org.foo
 
 // point in 3D space
 class Point {
-    float x, y, z
 
     public count_v(x, y, z){
+        x = x.toInteger()
+        y = y.toInteger()
+        z = z.toInteger()
         return x * y * z
     }
 }
