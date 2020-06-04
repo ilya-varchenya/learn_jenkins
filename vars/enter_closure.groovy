5e50605cb437badea94c47709e4b73446fb3e654
ilya-varchenya <ilya.varchenya@gmail.com>
2020-06-04 11:12:46 +0300

    logging #5

--- vars/enter_closure.groovy
+++ vars/enter_closure.groovy
@@ -1,14 +1,8 @@
 def call(Closure body) {
-    def parameters = body
-//    body.resolveStrategy = Closure.DELEGATE_FIRST
-//    body.delegate = parameters
 
     echo "111"
-
     node('master') {
         echo "222"
-        echo body.methods
-        echo "333"
         body()
     }
 }
