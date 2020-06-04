e6d42d3c908e5c5e7f96373d20e82ee2a09f6e4b
ilya-varchenya <ilya.varchenya@gmail.com>
2020-06-04 10:49:59 +0300

    comment delegation

--- vars/enter_closure.groovy
+++ vars/enter_closure.groovy
@@ -1,7 +1,7 @@
 def call(Closure body) {
     def parameters = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = parameters
+//    body.resolveStrategy = Closure.DELEGATE_FIRST
+//    body.delegate = parameters
 
     methods = parameters.methods
 
